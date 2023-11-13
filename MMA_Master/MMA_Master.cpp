185eb92b3c5d7ecba397f2e82c82559707efacd8
AlexH <a.hontanillab@alumnos.upm.es>
2023-11-13 19:22:01 +0100

    Comentarios de acelerómetro

--- MMA_Master/MMA_Master.cpp
+++ MMA_Master/MMA_Master.cpp
@@ -1,5 +1,7 @@
 #include "MMA_Master.h"
 
+//Funcion de configración del acelerómetro
+
 mma_control::mma_control(uint8_t address, int range){
 
     char reset[2] = {0x2B, 0x40};
@@ -16,24 +18,32 @@ mma_control::mma_control(uint8_t address, int range){
 
     char read_reset[1];
 
+    //Lectura del who am I
     if(i2c_driver.write(mma_addr, &(whoami), 1, true) != 0)
         printf("shiiiiit");
 
     i2c_driver.read(mma_addr, read_reset, 1);
 
+    //comprobación de who am I
     printf("%x", read_reset[0]);
 
+    //reset
     i2c_driver.write(mma_addr, reset, 2);
 
     i2c_driver.write(mma_addr, &(reset[0]), 1);
+    
+    //espera hasta que el reset se complete
     do{
         i2c_driver.read(mma_addr, read_reset, 1);
     }while(read_reset[0] == 0x40);
 
+    //Configuración de rango de medida (2g,4g,8g)
     i2c_driver.write(mma_addr, range_config, 2);
 
+    //Encendido del acelerómetro
     i2c_driver.write(mma_addr, activate, 2);
     
+    //Apagado de la cola FIFO (no la usamos)
     i2c_driver.write(mma_addr, fifo_off, 2);
 
 }
@@ -50,16 +60,10 @@ accel_vector mma_control::mma_read_vector(){
 
     i2c_driver.write(mma_addr, &status, 1);
 
+    //Lectura de los datos en bruto
     i2c_driver.read(mma_addr, data_raw, 7);
 
- //   i2c_driver.write(mma_addr, &data_addr, 1);
-
- //   i2c_driver.read(mma_addr, data_raw, 6);
-/*
-    int16_t unsigned_x = ((data_raw[1] >> 7) == 0) ? ((data_raw[1] << 8 | data_raw[2]) >> 2) : (((data_raw[1] << 8 | data_raw[2]) >> 2) | 0xC000);
-    int16_t unsigned_y = ((data_raw[3] >> 7) == 0) ? ((data_raw[3] << 8 | data_raw[4]) >> 2) : (((data_raw[3] << 8 | data_raw[4]) >> 2) | 0xC000);
-    int16_t unsigned_z = ((data_raw[5] >> 7) == 0) ? ((data_raw[5] << 8 | data_raw[6]) >> 2) : (((data_raw[5] << 8 | data_raw[6]) >> 2) | 0xC000);
-*/
+    //Operaciones para sacar los datos de forma correcta
 
     int16_t unsigned_x = ((data_raw[1] << 8 | data_raw[2]));
     unsigned_x >>= 2; 
